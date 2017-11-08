524aaf25a4642d61daaffc26aca45172789fb585
afaqbabar <afaqbabar@gmail.com>
2017-11-08 20:48:13 +0100

    Sign up in progress

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 import csv
 from flask import Flask,render_template, json, request
-
+from flask.ext.mysql import MySQL
 
 app = Flask(__name__)
+
+mysql = MySQL()
+
+app.config['MYSQL_DATABASE_USER']='root'
+app.config['MYSQL_DATABASE_PASSWORD']=''
+app.config['MYSQL_DATABASE_DB']='ArticleList'
+app.config['MYSQL_DATABASE_HOST']='localhost'
+mysql.init_app(app)
 
 '''
 def get_csv():
@@ -25,14 +33,31 @@ def showSignUp():
 
 @app.route('/signup',methods=['POST'])
 def signup():
-	_name = request.form['inputName']
-	_email = request.form['inputEmail']
-	_password = request.form['inputPassword']
+	try:
 
-	if _name and _email and _password:
-		return json.dumps({'html':'<span>All fields good !!</span>'})
-	else:
-		return json.dumps({'html':'<span> Enter required fields </span>'})
+		_name = request.form['inputName']
+		_email = request.form['inputEmail']
+		_password = request.form['inputPassword']
+	
+		if _name and _email and _password:
+			conn = mysql.connect()
+			cursor = conn.cursor()
+			_hashed_password = generate_password_hash(_password)
+			cursor.callproc('sp_createUser',(_name,_email,_hashed_password))
+			data = cursor.fetchall()
+			if len(data) is 0:
+				conn.commit()
+				return json.dumps({'message':'User created sucessfully'})
+			else:
+				return json.dumps({'error':str(data[0])})
+		else:
+			return json.dumps({'html':'<span>Enter the required fields</span>'})
+
+	except Exception as e:
+		return json.dumps({'error':str(e)})
+	finally:
+		cursor.close()
+		conn.close()
 
 if __name__ == '__main__':
 	app.run(debug=True, use_reloader=True)
