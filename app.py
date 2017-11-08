62f68aba6e552ca0235c26d644c6084a4e231c5e
afaqbabar <afaqbabar@gmail.com>
2017-11-08 17:29:03 +0100

    commited changes

--- app.py
+++ app.py
@@ -3,18 +3,20 @@ from flask import Flask,render_template
 
 app = Flask(__name__)
 
+'''
 def get_csv():
 	csv_path = './static/la-riots-deaths.csv'
 	csv_file = open(csv_path,'rb')
 	csv_obj = csv.DictReader(csv_file)
 	csv_list = list(csv_obj)
 	return csv_list
-
+'''
 @app.route("/")
 def index():
-	template = 'index.html'
-	object_list = get_csv()
-	return render_template(template, object_list=object_list)
+	#template = 'index1.html'
+	#object_list = get_csv()
+	#return render_template(template, object_list=object_list)
+	return render_template('index1.html')
 
 
 
