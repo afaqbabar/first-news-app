506e62824128c87dea1ce4675f1927fda8c16aba
afaqbabar <afaqbabar@gmail.com>
2017-11-08 20:06:20 +0100

    singup in progress

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import csv
-from flask import Flask,render_template
+from flask import Flask,render_template, json, request
+
 
 app = Flask(__name__)
 
@@ -18,7 +19,20 @@ def index():
 	#return render_template(template, object_list=object_list)
 	return render_template('index1.html')
 
+@app.route('/showSignUp')
+def showSignUp():
+	return render_template('signup.html')
 
+@app.route('/signup',methods=['POST'])
+def signup():
+	_name = request.form['inputName']
+	_email = request.form['inputEmail']
+	_password = request.form['inputPassword']
+
+	if _name and _email and _password:
+		return json.dumps({'html':'<span>All fields good !!</span>'})
+	else:
+		return json.dumps({'html':'<span> Enter required fields </span>'})
 
 if __name__ == '__main__':
 	app.run(debug=True, use_reloader=True)
