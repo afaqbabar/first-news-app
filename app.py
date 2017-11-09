36af8ecf40bb35d4d8fd930d0327abfe06189d70
afaqbabar <afaqbabar@gmail.com>
2017-11-09 13:05:51 +0100

    added article.html

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
-import csv
-from flask import Flask,render_template, json, request
+from flask import Flask,render_template,request
 from flask.ext.mysql import MySQL
-import werkzeug import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 
@@ -13,51 +11,30 @@ app.config['MYSQL_DATABASE_DB']='ArticleList'
 app.config['MYSQL_DATABASE_HOST']='localhost'
 mysql.init_app(app)
 
-'''
-def get_csv():
-	csv_path = './static/la-riots-deaths.csv'
-	csv_file = open(csv_path,'rb')
-	csv_obj = csv.DictReader(csv_file)
-	csv_list = list(csv_obj)
-	return csv_list
-'''
 @app.route("/")
 def index():
-	#template = 'index1.html'
-	#object_list = get_csv()
-	#return render_template(template, object_list=object_list)
-	return render_template('index1.html')
+	print("Entering Index function")
+	return render_template('article.html')
 
-@app.route('/showSignUp')
+@app.route('/showAddArticle')
 def showSignUp():
-	return render_template('signup.html')
+	print("Entering showAddArticle Function")
+	return render_template('addArticle.html')
 
-@app.route('/signUp',methods=['POST','GET'])
-def signup():
-	try:
-		_name = request.form['inputName']
-		_email = request.form['inputEmail']
-		_password = request.form['inputPassword']
+@app.route('/addArticle',methods=['POST'])
+def addWish():
+	print("Entering addArticle Function")
+	_title = request.form['inputTitle']
+	_description = request.form['inputDescription']
 	
-		if _name and _email and _password:
-			conn = mysql.connect()
-			cursor = conn.cursor()
-			_hashed_password = generate_password_hash(_password)
-			cursor.callproc('sp_CreateAuthor',(_name,_email,_hashed_password))
-			data = cursor.fetchall()
-			if len(data) is 0:
-				conn.commit()
-				return json.dumps({'message':'User created sucessfully'})
-			else:
-				return json.dumps({'error':str(data[0])})
-		else:
-			return json.dumps({'html':'<span>Enter the required fields</span>'})
-
-	except Exception as e:
-		return json.dumps({'error':str(e)})
-	finally:
-		cursor.close()
-		conn.close()
+	conn = mysql.connect()
+	cursor = conn.cursor()
+	cursor.callproc('sp_addWish',(_title,_description))
+	data = cursor.fetchall()
+	conn.commit()
+	return redirect('/showAddArticle')
+	cursor.close()
+	conn.close()
 
 if __name__ == '__main__':
 	app.run(debug=True, use_reloader=True)
