0dfd2a18a30cc24bb3dffc343a4e25c266b1aa49
afaqbabar <afaqbabar@gmail.com>
2017-11-09 16:57:52 +0100

    tested changes with db

--- app.py
+++ app.py
@@ -1,20 +1,32 @@
 from flask import Flask,render_template,request
-from flask_mysql import MySQL
+import pymysql
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-mysql = MySQL()
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://centos:centos@167.114.232.111/EmpData'
 
-app.config['MYSQL_DATABASE_USER']='root'
-app.config['MYSQL_DATABASE_PASSWORD']=''
-app.config['MYSQL_DATABASE_DB']='ArticleList'
-app.config['MYSQL_DATABASE_HOST']='localhost'
-mysql.init_app(app)
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 
+db = SQLAlchemy(app)
+
+'''
 @app.route("/")
 def index():
 	print("Entering Index function")
 	return render_template('article.html')
+'''
+
+
+@app.route('/')
+def testdb():
+    try:
+        db.session.query("1").from_statement("SELECT 1").all()
+        return '<h1>It works.</h1>'
+    except:
+        return '<h1>Something is broken.</h1>'
+
+
 
 @app.route('/showAddArticle')
 def showSignUp():
@@ -37,4 +49,4 @@ def addWish():
 	conn.close()
 
 if __name__ == '__main__':
-	app.run(debug=True, use_reloader=True)
+	app.run(debug=True)
--- local_test_db.py
+++ local_test_db.py
@@ -1,10 +1,12 @@
 import pymysql
 from flask import Flask 
 from flask_sqlalchemy import SQLAlchemy
+from flask import render_template
+
 
 app=Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost/ArticleList'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://test:test@167.114.232.111/EmpData'
 
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 
@@ -15,7 +17,7 @@ db = SQLAlchemy(app)
 def testdb():
     try:
         db.session.query("1").from_statement("SELECT 1").all()
-        return '<h1>It works.</h1>'
+        return render_template('article.html') 
     except:
         return '<h1>Something is broken.</h1>'
 
